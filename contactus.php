c6d2a21cc28cb6528d461a6fa94f9567e18f79a3
Jay Gohil <70004683+jay352@users.noreply.github.com>
2020-09-25 14:21:18 +0530

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,25 @@
+<<?php
+
+include "header.php";
+?>
+                <div class="container">
+                	<div class="row">
+                		<div class="col-md-8">
+ 							<br>
+ 							<br>               			
+                			<h1>CONATACT</h1>
+                			<br>
+                			<p style="text-align: justify;">
+                		Email us For More Information
+                		 </p>
+                		 <span><b>sparstechnology123@gmail.com</b></span>
+
+                		</div>
+                		<div class="col-md-4">
+                				
+                			</div>
+                	</div>
+                 
+		</div>
+</body>	
+</html>
